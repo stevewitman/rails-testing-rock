677743e1a817fb616af4cc353d84faae26c82ad8
Steve Witman <stevewitman@gmail.com>
2016-05-15 15:33:46 -0600

    used FactoryGirl to prepare data for tests and used RedCarpet markdown parser gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'puma'
 gem 'bootstrap-sass'
 gem 'simple_form'
+gem 'redcarpet'
 
 group :test do
   gem 'capybara'
--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -12,6 +12,11 @@ class AchievementsController < ApplicationController
     end
   end
 
+  def show
+    @achievement = Achievement.find(params[:id])
+    @description = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@achievement.description)
+  end
+
 private
 
   def achievement_params
